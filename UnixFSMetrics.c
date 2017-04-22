b69054cca6dfa675003e7acaeb42f88eb3f0d5b6
Jonathan Valencia <jonathan.valencia716@gmail.com>
2017-04-22 09:52:25 -0400

    mcreating a random file in the /tmp dir of size 1G

--- UnixFSMetrics.c
+++ UnixFSMetrics.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 unsigned long long int rdtsc(void)
 {
@@ -15,16 +17,19 @@ unsigned long long int rdtsc(void)
 }
 
 int main(int argc, char **argv){
-	unsigned long long a, b;
+
 	struct timespec start, end;
     long long unsigned int diff;
+
+    //create 1G file to test  
+    fprintf(stdout, "%s\n", "Please wait test file is being generated..." );
+    system("head -c 1G </dev/urandom > /tmp/testFile");
+    fprintf(stdout, "%s\n", "Test file has been created.");
 
 	//gets the starting time before system calls with error checking
     if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
         printf("Error reading the time for the start of the system call loop.");
     }
-
-	sleep(10);
 
 
 	//gets the ending time after system calls with error checking
@@ -34,6 +39,6 @@ int main(int argc, char **argv){
 
 	diff = 1000 * (end.tv_sec - start.tv_sec);
 	printf("%llu \n", diff );
-	
+
 	return 0;
 }
